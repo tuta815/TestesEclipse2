3dc528f020115fc745941a4b69b4b0a25049bc9e
tuta815 <arthur.imparato@hotmail.com>
2022-09-14 15:23:25 -0300

    encontro remoto 1

---
+++ TesteSenai/src/test/java/testesenai/TesteBuscaCursos.java
@@ -0,0 +1,36 @@
+package testesenai;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteBuscaCursos {
+private WebDriver driver;
+
+
+	@Before
+	public void abrirNavegador(){
+	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	}
+	
+/*	@Test
+	public void testeNavegador() {
+		driver.get("https://informatica.sp.senai.br/");
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("informatica");
+		driver.findElement(By.id("Busca1_btnBusca")).click();
+	}*/
+	
+	@Test
+	public void testeNavegador() {
+		driver.get("https://www.mercadolivre.com.br/");
+		driver.findElement(By.id("cb1-edit")).sendKeys("teclado");
+		driver.findElement(By.className("nav-search-btn")).click();
+	}
+}
